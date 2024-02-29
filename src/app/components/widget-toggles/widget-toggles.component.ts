dabda8f894629de981ee5120fc4c91bfd7167ff1
Nikesh <nikeshniki19@gmail.com>
2024-02-29 17:28:27 -0500

    light switch

--- src/app/components/widget-toggles/widget-toggles.component.ts
+++ src/app/components/widget-toggles/widget-toggles.component.ts
@@ -4,7 +4,7 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDividerModule} from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
-
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 @Component({
   selector: 'app-widget-toggles',
   standalone: true,
@@ -12,7 +12,8 @@ import { MatIconModule } from '@angular/material/icon';
     MatDividerModule,
     MatButtonModule,
     MatProgressBarModule,
-    MatIconModule
+    MatIconModule,
+    MatSlideToggleModule
   ],
   templateUrl: './widget-toggles.component.html',
   styleUrl: './widget-toggles.component.scss'
